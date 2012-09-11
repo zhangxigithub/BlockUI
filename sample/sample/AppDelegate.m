b7b93e1ae9c2bd5d6a35d0420796095f010826f9
zhangxi <zhangxi_1989@sina.com>
2012-09-11 10:12:39 +0800

    change sample

--- sample/sample/AppDelegate.m
+++ sample/sample/AppDelegate.m
@@ -17,12 +17,17 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 
     UIViewController *viewController = [[UIViewController alloc] init];
+    viewController.view.backgroundColor = [UIColor grayColor];
     self.window.rootViewController = viewController;
     [self.window makeKeyAndVisible];
     
     
     
+    
+    //***************
+    //***************
     UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [viewController.view addSubview:button];
     button.frame = CGRectMake(50, 50, 220, 44);
     [button setTitle:@"show alert view" forState:UIControlStateNormal];
     
@@ -39,42 +44,55 @@
         
     }];
     
-    [viewController.view addSubview:button];
+    
+    //***************
+    //***************
+    UISwitch *swithControl = [[UISwitch alloc] initWithFrame:CGRectMake(50, 10, 0, 0)];
+    [viewController.view addSubview:swithControl];
+    [swithControl handleControlEvent:UIControlEventValueChanged withBlock:^(id sender) {
+        UISwitch *s = sender;
+        NSLog(@"value:%@",s.isOn?@"on":@"off");
+    }];
+    
+    
+    //***************
+    //***************
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake(50, 110, 220, 0)];
+    [viewController.view addSubview:slider];
+    [slider handleControlEvent:UIControlEventValueChanged withBlock:^(id sender) {
+        UISlider *slider = sender;
+        NSLog(@"slider:%f",slider.value);
+    }];
+    
+    
+    
+    //***************
+    //***************
+    UISegmentedControl *segment = [[UISegmentedControl alloc] initWithItems:@[@"item1",@"item2",@"item3"]];
+    segment.frame = CGRectMake(50, 150, 220, 44);
+    [viewController.view addSubview:segment];
+    [segment handleControlEvent:UIControlEventValueChanged withBlock:^(id sender) {
+        UISegmentedControl *segment = sender;
+        NSLog(@"segment change to %d",segment.selectedSegmentIndex);
+    }];
     
     
     
     
+    //***************
+    //***************
+    
+    UIDatePicker *datePicker = [[UIDatePicker alloc] initWithFrame:CGRectMake(0, 244, 0, 0)];
+    [viewController.view addSubview:datePicker];
+    [datePicker handleControlEvent:UIControlEventValueChanged withBlock:^(id sender) {
+        UIDatePicker *picker = sender;
+        NSLog(@"date:%@",picker.date);
+    }];
     
     
     
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
 
 @end
